8db11de1f7bf07b57c55e76932d70d01a860ba1c
mambli-joe <anton.mambli@gmail.com>
2025-03-11 22:33:47 +0300

    save, restore, download

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,8 +1,8 @@
-import questionsList from "../../assets/questions/index.json";
-import { useState } from "react";
+import theory from "../../assets/questions/theory.json";
+import { useEffect, useState } from "react";
 
 const Questions = () => {
-  const [controlledQuestions, setControlledQuestions] = useState(questionsList);
+  const [controlledQuestions, setControlledQuestions] = useState(theory);
   const [selectedGroup, setSelectedGroup] = useState("all");
 
   console.log(
@@ -11,12 +11,29 @@ const Questions = () => {
     ),
   );
 
+  const resetQuestions = () => {
+    localStorage.removeItem("theory");
+    setControlledQuestions(theory);
+  };
+
+  const restoreQuestions = () => {
+    const restored = JSON.parse(localStorage.getItem("theory") || "[]");
+    if (restored?.length) {
+      setControlledQuestions(restored);
+    }
+  };
+
+  const saveQuestions = () => {
+    localStorage.setItem("theory", JSON.stringify(controlledQuestions));
+  };
+
   const updateRate = (id: number, rate: string) => {
     const copied = [...controlledQuestions];
     const candidate = copied.find((item) => item.id === id);
     candidate.rate = rate;
     console.log({ candidate, id, rate });
     setControlledQuestions(copied);
+    saveQuestions();
   };
 
   const getList = () => {
@@ -59,12 +76,46 @@ const Questions = () => {
   };
 
   const getGroupList = () => {
-    return Array.from(new Set(questionsList.map((item) => item.group)));
+    return Array.from(new Set(theory.map((item) => item.group)));
   };
 
+  const downloadResults = () => {
+    const extractedData = controlledQuestions.map((item) => ({
+      rate: item.rate,
+      question: item.question,
+    }));
+    const dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(JSON.stringify(extractedData));
+    const downloadAnchorNode = document.createElement("a");
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "results.json");
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  };
+
+  useEffect(restoreQuestions, []);
   return (
     <div>
       <div className="flex-col items-center text-base font-semibold text-gray-900 dark:text-white relative mb-8">
+        <div className={"flex mb-4 justify-between"}>
+          <button
+            onClick={downloadResults}
+            type="button"
+            className="cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Download results
+          </button>
+          <button
+            onClick={resetQuestions}
+            type="button"
+            className="cursor-pointer focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Reset
+          </button>
+        </div>
+
         <select
           id="group"
           onChange={(e) => setSelectedGroup(e.target.value)}
@@ -113,13 +164,24 @@ const Questions = () => {
                       onChange={(e) => updateRate(item.id, e.target.value)}
                       className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                     >
-                      <option value="default" selected>
+                      <option
+                        value="default"
+                        selected={item.rate === "default"}
+                      >
                         Не задан
                       </option>
-                      <option value="pass">Не ответил</option>
-                      <option value="bad">Плохо</option>
-                      <option value="good">Хорошо</option>
-                      <option value="great">Отлично</option>
+                      <option selected={item.rate === "pass"} value="pass">
+                        Не ответил
+                      </option>
+                      <option selected={item.rate === "bad"} value="bad">
+                        Плохо
+                      </option>
+                      <option selected={item.rate === "good"} value="good">
+                        Хорошо
+                      </option>
+                      <option selected={item.rate === "great"} value="great">
+                        Отлично
+                      </option>
                     </select>
                   </div>
                 </div>
